57d825d3963924aa51f545c99d1fcf8cea07ec91
Kohei Tanaka <kohei69@gmail.com>
2022-12-22 23:49:25 +0900

    fix

--- backend/src/components/posts/post.resolvers.ts
+++ backend/src/components/posts/post.resolvers.ts
@@ -1,4 +1,4 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 import { PostModel } from './interfaces/post.model';
 
 @Resolver((of) => PostModel)
