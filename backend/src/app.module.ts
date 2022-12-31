f0d08a413be3c46b529c7e0b5c178d08f0be807a
Kohei Tanaka <kohei69@gmail.com>
2022-12-31 12:07:05 +0900

    refactor app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { PostsModule } from '@pd-components/posts/posts.module';
-import * as path from 'path';
 import { PbEnvModule } from '@pd-config/environments/pb-env.module';
+import { PbEnv } from '@pd-config/environments/pb-env.service';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
+    GraphQLModule.forRootAsync({
+      inject: [PbEnv],
+      useFactory: (env: PbEnv) => env.GqlModuleOptionsFactory,
     }),
     PostsModule,
     PbEnvModule,
--- backend/src/config/environments/pb-env.service.ts
+++ backend/src/config/environments/pb-env.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { GqlModuleOptions } from '@nestjs/graphql';
+import * as path from 'path';
 
 /**
  * アプリケーションモジュールで利用する設定値は、ここから取得します。
@@ -10,6 +12,32 @@ export class PbEnv {
 
   isProduction(): boolean {
     return this.configService.get('NODE_ENV') === 'production';
+  }
+
+  get GqlModuleOptionsFactory(): GqlModuleOptions {
+    // 開発：コードからスキーマを生成し、Playgroundも利用する。
+    // バックエンドのコードが正なのでコードファーストアプローチを使う
+    const devOptions: GqlModuleOptions = {
+      autoSchemaFile: path.join(
+        process.cwd(),
+        'src/generated/graphql/schema.gql',
+      ),
+      sortSchema: true,
+      debug: true,
+      playground: true,
+    };
+
+    // 本番環境：実行だけ
+    const prdOptions: GqlModuleOptions = {
+      autoSchemaFile: true,
+      debug: false,
+      playground: false,
+    };
+    if (this.isProduction()) {
+      return prdOptions;
+    } else {
+      return devOptions;
+    }
   }
 
   get service() {
