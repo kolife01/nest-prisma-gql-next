5e7aa58ea4cfe0780fb181853b5d1f8842ce5cda
Kohei Tanaka <kohei69@gmail.com>
2023-01-02 16:27:44 +0900

    add tf main

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = "~> 1.0.0"
+  backend "gcs" {
+    prefix = "tfstate/v1"
+  }
+}
+
+## project ##
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.primary_region
+}
